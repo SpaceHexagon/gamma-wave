2ced85655a060e6b3eeda9bce4278b6dddd923a3
Jeremy Evans <openspacehexagon@gmail.com>
2017-10-05 22:25:06 -0400

    saving my eyes

--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'Gamma Wave',
+    description: 'Media player and server, creating oscillations pleasing to humans',
+    keywords: 'media player, mongodb, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   db: {
--- modules/core/client/directives/page-title.client.directive.js
+++ modules/core/client/directives/page-title.client.directive.js
@@ -18,7 +18,7 @@
       $rootScope.$on('$stateChangeSuccess', listener);
 
       function listener(event, toState) {
-        var applicationCoreTitle = 'MEAN.js',
+        var applicationCoreTitle = 'Gamma Wave',
           separator = ' - ',
           stateTitle = applicationCoreTitle + separator;
 
